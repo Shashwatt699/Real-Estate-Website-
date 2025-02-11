9092d7455e395a64045d74fdf07e00ddd9ece5a8
Shashwatt699 <shashwatsaxena4545@gmail.com>
2025-02-11 11:22:14 +0530

    Good Start

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { Router , Routes , Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import { Link } from 'react-router-dom';
+import Properties from './Pages/Properties';
+import PropertyDetail from './Pages/PropertyDetail';
+import UserDashboard from './Pages/UserDashboard';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="text-center p-8">
+        <nav>
+          <Link to="/Home" className="text=white mr-4">Home</Link>
+          <Link to="/properties" className='text-white'>Properties</Link>
+          <Link to="/PropertyDetail" className='text-white'>PropertyDetail</Link>
+          <Link to="/UserDashboard" className='text-white'>UserDashboard</Link>
+        </nav>
+        <Routes>
+          <Route path="/Home" element={<Home />} />
+          <Route path="/properties" element={<Properties />} />
+          <Route path="/PropertyDetail" element={<PropertyDetail />} />
+          <Route path="/UserDashboard" element={<UserDashboard />} />
+        </Routes>
 
-export default App
+    </div>
+  );
+};
+
+export default App;
